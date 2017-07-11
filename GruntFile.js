2219914e4b8a451b232f7e56d9c383fc436ef554
Zia <zia_gt@yahoo.com>
2017-07-11 22:15:46 +0200

    Uglify added for JavaScript

--- GruntFile.js
+++ GruntFile.js
@@ -10,12 +10,23 @@ module.exports = function (grunt) {
                 src: ['resources/javascripts/Main.js', 'resources/javascripts/Controller.js'],
                 dest: 'resources/javascripts/bin/<%= pkg.name %>.js',
             }
-
+        },
+        uglify: {
+            options: {
+            mangle:false
+            },
+            my_target: {
+            files: {
+                'resources/javascripts/bin/<%= pkg.name %>.min.js': ['resources/javascripts/bin/<%= pkg.name %>.js']
+            }
+            }
         }
+
     });
 
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     //Tasks
     grunt.registerTask('default', ['concat']);
