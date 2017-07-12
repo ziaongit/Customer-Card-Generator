dab31045d2fff90b88a6c3bf04c784e7edf34624
Zia <zia_gt@yahoo.com>
2017-07-12 02:26:04 +0200

    JavaScript Mockup added

--- resources/javascripts/bin/customer-card-generator.js
+++ resources/javascripts/bin/customer-card-generator.js
@@ -1,3 +1,38 @@
-(function (){
-    console.log('JavaScript');
-})();
+var db = [
+    {name: 'Zia', email:'zia_gt@yahoo.com', age:30},
+    {name: 'Mehmood', email:'mehmood_123t@gmail.com', age:23},
+    {name: 'Fatima', email:'canic_fatima@yahoo.com', age:20}
+];
+
+(function Avatars(db){
+    this.init = function(){
+        this.generateList();
+    };
+
+
+    this.generateList = function(){
+        var parent = document.querySelector('#parent_avatars');
+        var template = '';
+
+        for(var i=0; i < db.length; i++){
+            template += '<div class="col-sm-4">';
+            template +=     '<div class="card">';
+            template +=         '<div class="card-delete" data-card="">x</div>';
+            template +=         '<div class="card-block">';
+            template +=             '<h3 class="card-title">'+ db[i].name +'</h3>';
+            template +=             '<p class="card-text"><strong>Email:</strong> '+ db[i].email +'</p>';
+            template +=             '<p class="card-text"><strong>Age:</strong> '+ db[i].age +'</p>';
+            template +=          '</div>';
+            template +=     '</div>';
+            template += '</div>';
+        }
+
+        parent.innerHTML = '';
+        parent.insertAdjacentHTML('afterbegin', template);
+
+    };
+
+
+
+    this.init();
+})(db);
